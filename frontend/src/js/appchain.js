7082199a167754fca5c76e0a1bd5524bd0f0367a
Ricardo Prins <ricardoprins@gmail.com>
2020-08-28 12:42:13 -0600

    more blockchain stuff

--- frontend/src/js/appchain.js
+++ frontend/src/js/appchain.js
@@ -6,18 +6,19 @@ AppChain = {
     "0xb0c4df6950ef538df20f89d4857842fb93b8343931a416d127173cad7cca9345", //demo signer's off chain private key (not present in any node) [address - 0xB131dFD7Bd2edfACCf04531A8181da0EAa1641Ad]
   // pass live blockchain election contract address here
   contractAddress: "0xC88A235f6E63384DB9f4075A6AffD53CEB5794aB", //  deployed demo contract Address on mumbai testnet
-
+  signerPublicKey: "0xB131dFD7Bd2edfACCf04531A8181da0EAa1641Ad", //signer's public key
+  electionId: 0, //initialized
   // pass live blockchain factory contract address here
-  factorycontractAddress: "0x241512C692Dea2eA84B13F706Eca8E3424881C2f",
+  factoryContractAddress: "0x241512C692Dea2eA84B13F706Eca8E3424881C2f",
 
   init: function () {
-    return App.initWeb3();
+    return AppChain.initWeb3();
   },
 
   initWeb3: function () {
     // Modern dapp browsers...
     if (window.ethereum) {
-      App.web3Provider = window.ethereum;
+      AppChain.web3Provider = window.ethereum;
       try {
         // Request account access
         window.ethereum.enable();
@@ -26,42 +27,42 @@ AppChain = {
         console.error("User denied account access");
       }
     }
-    // Legacy dapp browsers...
+    // Legacy dAppChain browsers...
     else if (window.web3) {
       window.web3.currentProvider.enable();
-      App.web3Provider = window.web3.currentProvider;
+      AppChain.web3Provider = window.web3.currentProvider;
     }
     // If no injected web3 instance is detected, fall back to Ganache
     else {
-      App.web3Provider = new Web3.providers.HttpProvider(
+      AppChain.web3Provider = new Web3.providers.HttpProvider(
         "http://localhost:7545"
       );
     }
-    web3 = new Web3(App.web3Provider);
+    web3 = new Web3(AppChain.web3Provider);
   },
 
   initContract: function () {
     $.getJSON("Election.json", function (election) {
       // Instantiate a new truffle contract from the artifact
-      App.contracts.Election = TruffleContract(election);
+      AppChain.contracts.Election = TruffleContract(election);
       // Connect provider to interact with contract
-      App.contracts.Election.setProvider(App.web3Provider);
-      App.listenForEvents(); // event listener call for votecasting event
+      AppChain.contracts.Election.setProvider(AppChain.web3Provider);
+      AppChain.listenForEvents(); // event listener call for votecasting event
     });
 
     $.getJSON("ElectionFactory.json", function (factory) {
       // Instantiate a new truffle contract from the artifact
-      App.contracts.ElectionFactory = TruffleContract(factory);
+      AppChain.contracts.ElectionFactory = TruffleContract(factory);
       // Connect provider to interact with contract
-      App.contracts.ElectionFactory.setProvider(App.web3Provider);
+      AppChain.contracts.ElectionFactory.setProvider(AppChain.web3Provider);
     });
 
-    return App.render();
+    return AppChain.render();
   },
 
   //event listener function for Vote casting
   listenForEvents: function () {
-    App.contracts.Election.deployed().then(function (instance) {
+    AppChain.contracts.Election.deployed().then(function (instance) {
       instance
         .votedEvent(
           {},
@@ -78,40 +79,38 @@ AppChain = {
 
   render: function () {
     // Load account data
-    var account = web3.eth.getAccounts()[0];
+    var account = web3.eth.accounts[0];
+    console.log(account);
 
-    App.account = account;
+    AppChain.account = account;
     $("#accountAddress").html("Your Account: " + account);
 
     //console.log("accounts[1] - " + web3.eth.accounts[1]);
   },
 
-  castVote: function () {
+  authorizeBallotTransaction: function () {
     // function is called  during vote casting
-    var candidateId = 2;
-    var cID = [1, 2];
-    var sig = [];
-    console.log(cID[0], " + ", cId[1]);
-    for (var i = 0; i < cID.length; i++) {
-      var blindSig = App.PrepareBlindVote(cID[i], App.contractAddress);
-      console.log("Returned signature : " + blindSig);
-      sig[i] = blindSig;
-    }
+    var candidateId = 2; //demo id initialied just for demonstration
 
-    App.contracts.Election.deployed()
+    var blindSig = AppChain.PrepareBlindVote(
+      candidateId,
+      AppChain.contractAddress
+    );
+    console.log("Returned signature : " + blindSig);
+
+    AppChain.contracts.Election.deployed()
       .then(function (instance) {
-        return instance.vote(...cID, ...sig, { from: App.account });
+        return instance.vote(candidateId, blindSig, { from: AppChain.account }); //call with individual ballot data (will send bulk in future)
       })
       .then(function (result) {
         // Wait for votes to update
         //Reload everything when a new vote is recorded
-        App.init();
+        AppChain.init();
       })
       .catch(function (err) {
         console.error(err);
       });
   },
-
   PrepareBlindVote: function (candidateId, contractAdd) {
     var hash = web3.utils.soliditySha3(
       { type: "uint", value: candidateId },
@@ -119,13 +118,13 @@ AppChain = {
     ); // calculate the sha3 of given input parameters in the same way solidity would (arguments will be ABI converted and tightly packed before being hashed)
 
     console.log("Original HASH " + hash);
-    var signature = App.signBlindVote(hash);
+    var signature = AppChain.signBlindVote(hash);
     console.log("HI Signature " + signature);
     return signature;
   },
 
   signBlindVote: function (hash) {
-    var signObject = web3.eth.accounts.sign(hash, App.signerAccount);
+    var signObject = web3.eth.accounts.sign(hash, AppChain.signerAccount);
     console.log("Original message " + signObject.message);
     console.log("Original messagehash " + signObject.messageHash);
     console.log("Original Signature " + signObject.signature);
@@ -136,9 +135,9 @@ AppChain = {
     // function is called  during voter registration
     var voterNationalID = $("#voterNID").val();
     console.log(voterNationalID);
-    App.contracts.Election.deployed()
+    AppChain.contracts.Election.deployed()
       .then(function (instance) {
-        return instance.addVoter(voterNationalID, { from: App.account });
+        return instance.addVoter(voterNationalID, { from: AppChain.account });
       })
       .then(function (result) {
         // Wait for votes to update
@@ -146,7 +145,28 @@ AppChain = {
         $("#loader").show();
 
         //Reload everything when a new voter is created
-        App.init();
+        AppChain.init();
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  },
+  populateElection: function () {
+    // function is called  during vote casting
+    AppChain.electionId = AppChain.electionId + 1; // increment global variable value (is this how javascript integer increment work?)
+
+    AppChain.contracts.ElectionFactory.deployed()
+      .then(function (instance) {
+        return instance.createNewElection(
+          AppChain.signerPublicKey,
+          AppChain.electionId,
+          { from: AppChain.account }
+        ); //call with individual ballot data (will send bulk in future)
+      })
+      .then(function (result) {
+        // Wait for votes to update
+        //Reload everything when a new vote is recorded
+        AppChain.init();
       })
       .catch(function (err) {
         console.error(err);
