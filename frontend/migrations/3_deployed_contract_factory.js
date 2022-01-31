9e33fdded1ed22e30c53538bdfeb95fb8d609049
Ricardo Prins <ricardoprins@gmail.com>
2022-01-31 15:42:15 -0700

    revamping frontend to reduce vulnerabilities

--- frontend/migrations/3_deployed_contract_factory.js
+++
@@ -1,5 +0,0 @@
-var Factory = artifacts.require("ElectionFactory");
-
-module.exports = function(deployer) {
-    deployer.deploy(Factory);
-};
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,25 +1,7 @@
 import React from "react";
 
 function Home() {
-  return (
-    <div className="home">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="./banner.jpeg"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
-            <p>OutOfIdea voting platform is a nice voting platform.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
